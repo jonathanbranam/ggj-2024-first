90e2e20c11b4a2f92044cbba6562dd55547402dc
Jonathan Branam <jonathan.branam@gmail.com>
2024-01-26 16:08:59 -0500

    Red ground

--- src/firstStep.js
+++ src/firstStep.js
@@ -2,34 +2,38 @@
 var BABYLON = require("@babylonjs/core");
 
 var createScene = function (engine, canvas) {
-    // This creates a basic Babylon Scene object (non-mesh)
-    var scene = new BABYLON.Scene(engine);
+  // This creates a basic Babylon Scene object (non-mesh)
+  var scene = new BABYLON.Scene(engine);
 
-    // This creates and positions a free camera (non-mesh)
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+  // This creates and positions a free camera (non-mesh)
+  var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
 
-    // This targets the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
+  // This targets the camera to scene origin
+  camera.setTarget(BABYLON.Vector3.Zero());
 
-    // This attaches the camera to the canvas
-    camera.attachControl(canvas, true);
+  // This attaches the camera to the canvas
+  camera.attachControl(canvas, true);
 
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+  // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+  var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
 
-    // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
+  // Default intensity is 1. Let's dim the light a small amount
+  light.intensity = 0.7;
 
-    // Our built-in 'sphere' shape.
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
+  // Our built-in 'sphere' shape.
+  var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
 
-    // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
+  // Move the sphere upward 1/2 its height
+  sphere.position.y = 1;
 
-    // Our built-in 'ground' shape.
-    var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
+  // Our built-in 'ground' shape.
+  var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
 
-    return scene;
+  const groundMat = new BABYLON.StandardMaterial("Ground Mat", scene);
+  groundMat.diffuseColor = BABYLON.Color3.Red();
+  ground.material = groundMat;
+
+  return scene;
 };
 
 module.exports = {
